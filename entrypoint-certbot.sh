e9b84e0ce8161448fb278cd5ee351a17ba395dfe
arnold <arnold@jasny.net>
2024-12-11 14:32:04 +0100

    Fix calling random-sleep in cron

--- entrypoint-certbot.sh
+++ entrypoint-certbot.sh
@@ -13,7 +13,7 @@ if [ -n "$CERTBOT_EMAIL" ]; then
   echo "/bin/sh /usr/local/bin/cron-certbot.sh > /var/log/cron-certbot.log 2>&1" | at now + 1 minute
 
   # Add a cron job to run the certbot script twice a day (every 12 hours) with the 'renew' argument
-  echo "0 0,12 * * * bin/sh random-sleep 3600; /bin/sh /usr/local/bin/cron-certbot.sh renew > /var/log/cron-certbot.log 2>&1" > /etc/crontabs/root
+  echo "0 0,12 * * * bin/sh /usr/local/bin/random-sleep.sh 3600; /bin/sh /usr/local/bin/cron-certbot.sh renew > /var/log/cron-certbot.log 2>&1" > /etc/crontabs/root
 
   # Start the cron daemon in the background
   crond &
